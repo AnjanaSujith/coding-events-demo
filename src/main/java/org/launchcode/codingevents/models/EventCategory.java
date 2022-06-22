509198ec6f1edc6b586e2f8fefbea8885cad0cdd
anjan <anjanasujith@gmail.com>
2022-06-22 21:06:57 +0530

    Completed @ManyToOne and @OneTo Many relationship code

--- src/main/java/org/launchcode/codingevents/models/EventCategory.java
+++ src/main/java/org/launchcode/codingevents/models/EventCategory.java
@@ -1,7 +1,10 @@
 package org.launchcode.codingevents.models;
 
 import javax.persistence.Entity;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -12,6 +15,9 @@ public class EventCategory extends AbstractEntity {
     @Size(min=3, message="Name must be at least 3 characters long")
     private String name;
 
+    @OneToMany(mappedBy ="eventCategory")
+    private final List<Event> events = new ArrayList<>();
+
     public EventCategory(@Size(min = 3, message = "Name must be at least 3 characters long") String name) {
         this.name = name;
     }
@@ -20,6 +26,10 @@ public class EventCategory extends AbstractEntity {
 
     public String getName() {
         return name;
+    }
+
+    public List<Event> getEvents() {
+        return events;
     }
 
     public void setName(String name) {
